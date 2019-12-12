d3e16b313c5dcae328309d852d93f95d8a138892
dove <ndoverte11@gmail.com>
2019-12-12 14:41:45 +0800

    Updated schema.sql and started working on add expenses page

--- admin/private/functions.php
+++ admin/private/functions.php
@@ -1,10 +1,5 @@
 <?php 
 
-// function is_post_request() {
-//     return $_SERVER['REQUEST_METHOD'] == 'POST';
-//   }
-
-
-function hello() {
-    return "hello";
+function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
 }
--- schema.sql
+++ schema.sql
@@ -6,6 +6,8 @@ CREATE TABLE users (
   fullname VARCHAR(255),
   username VARCHAR(255),
   email VARCHAR(255),
+  user_image TEXT,
+  user_bio TEXT,
   mobile_number BIGINT,
   hashed_password VARCHAR(255),  
   reg_date TIMESTAMP,  
@@ -24,7 +26,17 @@ CREATE TABLE userexpense (
 );
 
 
--- for dummy data try this
+-- for user dummy data
 
 INSERT INTO users (fullname, username, email, mobile_number, hashed_password, reg_date ) VALUES ('John Taylor', 'john', 'john@gmail.com', '09453567891', 'bdjsbsdgsdg', 'now()');
 INSERT INTO users (fullname, username, email, mobile_number, hashed_password, reg_date ) VALUES ('Howard Petersen', 'howard', 'hp_01@gmail.com', '09215678974', 'xssdgsgga', 'now()');
+
+-- for userexpense dummy data
+
+INSERT INTO userexpense (user_id, expense_date, expense_item, expense_cost, note_date) VALUES (2, '2019-12-12', 'Car', '1200', '2019-12-12 10:06:19');
+INSERT INTO userexpense (user_id, expense_date, expense_item, expense_cost, note_date) VALUES (2, '2019-12-01', 'Furnitures', '5000', '2019-12-01 05:50:01');
+INSERT INTO userexpense (user_id, expense_date, expense_item, expense_cost, note_date) VALUES (2, '2019-12-02', 'Food', '50', '2019-12-02 03:30:09');
+
+INSERT INTO userexpense (user_id, expense_date, expense_item, expense_cost, note_date) VALUES (3, '2019-12-06', 'Milk', '100', '2019-12-06 12:20:45');
+INSERT INTO userexpense (user_id, expense_date, expense_item, expense_cost, note_date) VALUES (3, '2019-12-07', 'Vagetables', '70', '2019-12-06 08:10:09');
+INSERT INTO userexpense (user_id, expense_date, expense_item, expense_cost, note_date) VALUES (3, '2019-12-08', 'Rent', '75', '2019-12-06 05:36:59');
