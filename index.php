f67b5bdbfac4a807131499319ab77eb145303f78
mh3mh3 <merilyn.merida@gmail.com>
2019-12-12 15:58:17 +0800

    index.php: remove line 19-40 to connect header.php and edit code to line 1 - 16, header.php: add code to line 17 and 22

--- index.php
+++ index.php
@@ -1,46 +1,22 @@
-<?php 
+<?php
 
-require_once 'admin/private/database.php';
-
-
+include 'admin/private/database.php';
+include 'admin/inc/header.php';
+include 'admin/private/functions.php';
+$valid = false;
+$msg = "";
 if(isset($_POST['submit'])){
-  $pmail = $_POST['email'];
-  $password = $_POST['hashed_password'];
-
-
-$query = "INSERT INTO tbluser(username, password) VALUES('$username', '$password')";
-
-$result = mysqli_query($dbconnection, $query);
-
-if(!$result){
-  die(mysqli_error());
+  $msg = createUser();
+  if(strlen($msg) > 0){
+    $valid = true;
+  }else{
+    $valid = false;
+  }
 }
-
-}
- ?> 
+?>
 
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin 2 - Login</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="admin/fonts/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="admin/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
 
 <body class="bg-gradient-primary">
 
