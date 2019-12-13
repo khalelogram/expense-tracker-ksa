0c2e57a2cac28cab0bd9774adfc642a11c012a14
mh3mh3 <merilyn.merida@gmail.com>
2019-12-13 14:09:06 +0800

    function.php: adding code to line 10 - 90. Just try the code to work.

--- register.php
+++ register.php
@@ -1,27 +1,17 @@
+<?php
+
+// include 'admin/private/database.php';
+include 'admin/inc/header.php';
+// include 'admin/private/functions.php';
+include 'admin/private/init.php';
 
 
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
+?>  
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
 
-  <title>SB Admin 2 - Register</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="admin/fonts/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="admin/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
 
 <body class="bg-gradient-primary" style="overflow:hidden">
 
@@ -37,27 +27,27 @@
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
               </div>
-              <form class="user" action="process.php" method="POST">
+              <form class="user" action="admin/private/init.php" method="POST">
                 <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="fullname" placeholder="Full Name" name="Fullname">
+                  <input type="text" class="form-control form-control-user" id="fullname" placeholder="Full Name" name="fullname">
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" class="form-control form-control-user" id="username" placeholder="Username" name="Username">
+                    <input type="text" class="form-control form-control-user" id="username" placeholder="Username" name="username">
                   </div>
                   <div class="col-sm-6">
-                    <input type="number" class="form-control form-control-user" id="mobile" placeholder="Mobile Number" name="MobileNumber">
+                    <input type="int" class="form-control form-control-user" id="mobile" placeholder="Mobile Number" name="mobile_number">
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="email" placeholder="Email Address" name="Email">
+                  <input type="email" class="form-control form-control-user" id="email" placeholder="Email Address" name="email">
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="Password">
+                    <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="hashed_password">
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="password" placeholder="Repeat Password" name="Cpass">
+                    <input type="password" class="form-control form-control-user" id="password" placeholder="Repeat Password" name="cpass">
                   </div>
                 </div>
                  <input type="submit" class="btn btn-primary btn-user btn-block" value="Register Account">
