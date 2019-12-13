24ff32eb6976d7c3b577b3eb163461846c5d88e7
esciison19 <esciison19@gmail.com>
2019-12-13 10:47:10 +0800

    bug fic

--- admin/private/init.php
+++ admin/private/init.php
@@ -1,7 +1,7 @@
 <?php
   ob_start(); // output buffering is turned on
   session_start(); // turn on sessions
-
+  error_reporting(0);
   require_once('database.php');
   require_once('functions.php');
 
