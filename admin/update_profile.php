2a73ea2cc28eee30bc89275b6a91c1be38bde1df
ericcuys <eric.cuysona@gmail.com>
2019-12-15 07:03:56 +0800

    feature/user_profille created update_profile for testing purposes

---
+++ admin/update_profile.php
@@ -0,0 +1,105 @@
+<?php
+	require_once('private/init.php');
+	global $db;
+	$email = $_POST['email'];
+	$mobile_number = $_POST['mobile_number'];
+	$fullname = $_POST['fullname'];
+	$user_bio = $_POST['user_bio'];
+	// $user_photo = $_POST['user_photo'];
+	$userid = $_POST['userid'];
+
+	/*$target_dir = "img/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+	    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	    if($check !== false) {
+	        echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+	    echo "Sorry, file already exists.";
+	    $uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+	    echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+	    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+		move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}*/
+
+
+	$currentDir = getcwd();
+    $uploadDirectory = "";
+
+    $errors = []; // Store all foreseen and unforseen errors here
+
+    $fileExtensions = ['jpeg','jpg','png']; // Get all the file extensions
+
+    $fileName = $_FILES['myfile']['name'];
+    $fileSize = $_FILES['myfile']['size'];
+    $fileTmpName  = $_FILES['myfile']['tmp_name'];
+    $fileType = $_FILES['myfile']['type'];
+    $fileExtension = strtolower(end(explode('.',$fileName)));
+
+    $uploadPath = $currentDir . $uploadDirectory . basename($fileName); 
+
+    if (isset($_POST['submit'])) {
+
+        if (! in_array($fileExtension,$fileExtensions)) {
+            $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+        }
+
+        if ($fileSize > 2000000) {
+            $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+        }
+
+        if (empty($errors)) {
+            $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+            if ($didUpload) {
+                echo "The file " . basename($fileName) . " has been uploaded";
+            } else {
+                echo "An error occurred somewhere. Try again or contact the admin";
+            }
+        } else {
+            foreach ($errors as $error) {
+                echo $error . "These are the errors" . "\n";
+            }
+        }
+    }
+
+	die();
+	$sql = "UPDATE users SET email='".$email."', mobile_number='".$mobile_number."', user_bio='".$user_bio."', fullname='".$fullname."', user_image='".$_FILES['myfile']['name']."' WHERE id='".$userid."';";
+
+	if (mysqli_query($db, $sql)) {
+		echo "<script>alert('Profile Successfully Updated!');document.location.href='../admin/user_profile.php';</script>";
+	}else{
+		echo "Error updating record: " . mysqli_error($db);
+	}
+	mysqli_close($db);
+?>
