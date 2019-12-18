45a759818371a648cd95bc255d72bdc865e1d44c
dove <ndoverte11@gmail.com>
2019-12-18 14:32:00 +0800

    Updated dashbaord V2

--- admin/private/db_query_function.php
+++ admin/private/db_query_function.php
@@ -5,13 +5,12 @@ function insert_expenses($expense_item) {
   $query = "INSERT INTO userexpense ";
   $query .= "(user_id, expense_date, expense_item, expense_cost, note_date) ";
   $query .= "VALUES (";
-  $query .= "'2', ";
+  $query .= "'" . $_SESSION['user_id'] . "', ";
   $query .= "'" . __escape_string($expense_item['date']) . "', ";
   $query .= "'" . __escape_string($expense_item['item']) . "', ";
   $query .= "'" . __escape_string($expense_item['costitem']) . "', ";
   $query .= "now()";
   $query .= ")";
-  // echo $query;
   $result = mysqli_query($db, $query);
   check_query_from_db($result);
 }
@@ -20,9 +19,8 @@ function insert_expenses($expense_item) {
 function show_user_expense() {
   global $db;
   $query = "SELECT * FROM userexpense ";
-  $query .= " WHERE user_id = 2 ";
+  $query .= "WHERE user_id = '" . $_SESSION['user_id'] . "'";
   $query .= "ORDER BY expense_item ASC";
-
   $result = mysqli_query($db, $query);
   check_query_from_db($result);
   return $result;
@@ -41,11 +39,11 @@ function delete_user_expense($id) {
 }
 
 
-function show_all_today_and_yesterday($expense) {
+function show_all_today_and_yesterday($date) {
   global $db;
   $query = "SELECT SUM(expense_cost) AS 'cost' ";
   $query .= "FROM userexpense ";
-  $query .= "WHERE expense_date = '" . $expense . "'";
+  $query .= "WHERE expense_date = '" . $date . "' AND user_id = '" . $_SESSION['user_id'] . "'";
   $result = mysqli_query($db, $query);
   check_query_from_db($result);
   return $result;
@@ -56,7 +54,7 @@ function show_all_week_and_month_exp($past_date, $current_date) {
   global $db;
   $query = "SELECT SUM(expense_cost) AS 'cost' ";
   $query .= "FROM userexpense ";
-  $query .= "WHERE expense_date BETWEEN'" . $past_date . "' AND '" . $current_date . "'";
+  $query .= "WHERE expense_date BETWEEN'" . $past_date . "' AND '" . $current_date . "'  AND user_id = '" . $_SESSION['user_id'] . "'";
   $result = mysqli_query($db, $query);
   check_query_from_db($result);
   return $result;
@@ -86,38 +84,38 @@ function find_username($username) {
 }
 
 // start of expenses report functions
-function show_daily_report($fdate,$tdate){
-  global $db;
-  $query = "SELECT expense_date,SUM(expense_cost) AS totaldaily FROM `userexpense`
-            WHERE (expense_date BETWEEN '$fdate' AND '$tdate') 
-            GROUP BY expense_date";// && (user_id='$userid')
-​
-  $result = mysqli_query($db, $query);
-  check_query_from_db($result);
-  return $result;
-}
-​
-function show_monthly_report($fdate,$tdate){
-  global $db;
-  $query = "SELECT month(expense_date) AS rptmonth,year(expense_date) 
-            AS rptyear,SUM(expense_cost) AS totalmonth FROM userexpense  
-            WHERE (expense_date BETWEEN '$fdate' AND '$tdate') 
-            GROUP BY month(expense_date),year(expense_date)";// && (user_id='$userid')
+// function show_daily_report($fdate,$tdate){
+//   global $db;
+//   $query = "SELECT expense_date,SUM(expense_cost) AS totaldaily FROM `userexpense`
+//             WHERE (expense_date BETWEEN '$fdate' AND '$tdate') 
+//             GROUP BY expense_date";// && (user_id='$userid')
+// ​
+//   $result = mysqli_query($db, $query);
+//   check_query_from_db($result);
+//   return $result;
+// }
+// ​
+// function show_monthly_report($fdate,$tdate){
+//   global $db;
+//   $query = "SELECT month(expense_date) AS rptmonth,year(expense_date) 
+//             AS rptyear,SUM(expense_cost) AS totalmonth FROM userexpense  
+//             WHERE (expense_date BETWEEN '$fdate' AND '$tdate') 
+//             GROUP BY month(expense_date),year(expense_date)";// && (user_id='$userid')
             
-  $result = mysqli_query($db, $query);
-  check_query_from_db($result);
-  return $result;
-}
-​
-function show_yearly_report($fdate,$tdate){
-  global $db;
-  $query = "SELECT year(expense_date) AS rptyear,SUM(expense_cost) 
-            AS totalyear FROM userexpense  
-            WHERE (expense_date BETWEEN '$fdate' AND '$tdate') 
-            GROUP BY year(expense_date)";// && (user_id='$userid')
-​
-  $result = mysqli_query($db, $query);
-  check_query_from_db($result);
-  return $result;
-}
-// end of expenses report functions
+//   $result = mysqli_query($db, $query);
+//   check_query_from_db($result);
+//   return $result;
+// }
+// ​
+// function show_yearly_report($fdate,$tdate){
+//   global $db;
+//   $query = "SELECT year(expense_date) AS rptyear,SUM(expense_cost) 
+//             AS totalyear FROM userexpense  
+//             WHERE (expense_date BETWEEN '$fdate' AND '$tdate') 
+//             GROUP BY year(expense_date)";// && (user_id='$userid')
+// ​
+//   $result = mysqli_query($db, $query);
+//   check_query_from_db($result);
+//   return $result;
+// }
+// // end of expenses report functions
